ebe131a046da6a34e67241bd845f3e1907bc4837
sisidovski <arerreee@gmail.com>
2019-01-17 13:22:58 +0900

    Update layout g

--- index.js
+++ index.js
@@ -8,18 +8,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 const getFullURL = (req) => req.protocol + '://' + req.get('host') + req.originalUrl;
 
-const onsens = ['Dogo onsen', 'Akiu onsen', 'Hokkawa onsen']
+const onsens = [
+  {
+    name: 'Dogo onsen',
+    img: 'https://www.visitehimejapan.com/image/rendering/attraction_image/4211/trim.800/3/2?v=c156583acd94d8709ffccc3c46c3d9508004972d'
+  },
+  {
+    name: 'Akiu onsen',
+    img: 'https://d3hg7snzn13jf0.cloudfront.net/files/kanko/893/893--2038855060f0e02b053ba1a74ab26746.jpg'
+  },
+  {
+    name: 'Hokkawa onsen',
+    img: 'https://www.guidoor.jp/wordpress/wp-content/uploads/2017/07/e4718c0ab3eeec065f29283b1d0b7614.jpg'
+  }
+];
 
 const getOnsens = () => {
   const normalize = input => input.replace(/\sonsen$/, '').toLowerCase();
   return onsens.map(onsen => {
-    const normalized = normalize(onsen);
-    const url = `/paradise/${normalized}`;
+    const normalized = normalize(onsen.name);
+    const url = `/${normalized}`;
     return {
       id: normalized,
-      name: onsen,
+      name: onsen.name,
       url: url,
-      ampUrl: `${url}/amp`
+      ampUrl: `${url}/amp`,
+      img: onsen.img
     };
   });
 };
@@ -39,7 +53,7 @@ app.get('/', (req, res) => {
   res.render('list', data);
 });
 
-app.get('/paradise/:name/amp', (req, res) => {
+app.get('/:name/amp', (req, res) => {
   const name = req.params.name;
   const onsens = getOnsens();
 
@@ -53,7 +67,7 @@ app.get('/paradise/:name/amp', (req, res) => {
   res.render('amp-item', data)
 });
 
-app.get('/paradise/:name', (req, res) => {
+app.get('/:name', (req, res) => {
   const name = req.params.name
   const onsens = getOnsens();
 
