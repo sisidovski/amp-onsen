564c90cd3fbe42fa10aa936fdf164f0f1d581f76
sisidovski <arerreee@gmail.com>
2019-01-18 17:51:53 +0900

    Add search console file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));
 
